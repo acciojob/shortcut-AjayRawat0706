deed1937863d42cef84cf64eb6bb8327c0c4fe35
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-13 01:06:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function shortcut(s1, s2) {
-  // your code here
+  if(s1.length==0 || s2.length==0){
+	  return ''
+  }
+	let ans='';
+	ans+=s1.charAt(0)+s2.charAt(0);
+	return ans;
 }
 
 // Do not change the code below.
